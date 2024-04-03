9e1e055f530614dfc7451b95b7690517ec23ee63
anasmohammed361 <anasmohammed361@gmail.com>
2024-04-03 23:32:39 +0530

    editorjs

--- src/lib/components/EditorJs/Editor.svelte
+++ src/lib/components/EditorJs/Editor.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
+	import type { BlockToolConstructable } from '@editorjs/editorjs';
 	import { createEditor } from './editorjs';
 	import Header from '@editorjs/header';
 	import List from '@editorjs/list';
+
 	const { data, isReady, editor } = createEditor({
+		autofocus: true,
 		tools: {
-			header: Header,
-			list: List
+			header: {
+				class: Header as unknown as BlockToolConstructable,
+				config: {
+					placeholder: 'Enter a header',
+					levels: [1, 2, 3, 4, 5, 6],
+					defaultLevel: 1
+				},
+				inlineToolbar: true
+			},
+			list: {
+				class: List as any,
+				inlineToolbar: true
+			}
 		}
 	});
 </script>
 
 <section class="container">
-    <div class="prose" use:editor></div>
-
+	<div class="prose" use:editor></div>
 </section>
