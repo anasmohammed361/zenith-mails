a9156d68e0b4acfca89341ed68815c569dd9d560
anasmohammed361 <anasmohammed361@gmail.com>
2024-05-08 14:29:06 +0530

    setup sst

--- sst.config.ts
+++ sst.config.ts
@@ -10,12 +10,21 @@ export default $config({
 			providers: {
 				aws: {
 					profile: 'default',
-					region: 'us-east-1'
-				}
-			}
+					region: 'us-east-1',
+					
+				},
+				
+			},
+			
 		};
 	},
 	async run() {
-		new sst.aws.SvelteKit('ZenithWeb', {});
+		new sst.aws.SvelteKit('ZenithWeb', {
+			domain:{
+				name:'mail.webzenith.tech',
+				dns:false,
+				cert:'arn:aws:acm:us-east-1:120710952431:certificate/3c5546c2-6182-4663-9241-23335eea0d58'
+			}
+		});
 	}
 });
